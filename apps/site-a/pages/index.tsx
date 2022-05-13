f89e331e3136e5a4fff9432b5fc8f08ae3b6f38e
Matt Pham <matt@nava.io>
2022-05-13 00:23:15 -0700

    Trigger CI

--- apps/site-a/pages/index.tsx
+++ apps/site-a/pages/index.tsx
@@ -13,7 +13,7 @@ export function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome site-a 👋
+              Welcome site-a! 👋
             </h1>
           </div>
 
